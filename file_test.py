6eb0f893c88c146951e095d6dfd15181af47c8c8
Ubuntu <daniel@datascientest.com>
2023-03-18 00:35:08 +0000

    correction de synthaxe dans le code de test

--- file_test.py
+++ file_test.py
@@ -2,12 +2,12 @@ import pytest
 
 def test_calc_addition():
   # Fonction test du résultat de 2+4
-    output = 2+4
+    output = 2 + 4
     assert output == 6
 
 def test_calc_substraction():
   # Fonction test du résultat de 2-4
-    output = 2-4
+    output = 2 - 4
     assert output == -2
 
 def test_calc_multiply():
